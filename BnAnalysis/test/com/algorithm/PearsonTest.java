04a3bce7260578e0660a3269cb1f667986ec197b
wubdut <981128587@qq.com>
2017-03-01 14:45:31 +0800

    add pearson.csv

--- BnAnalysis/test/com/algorithm/PearsonTest.java
+++ BnAnalysis/test/com/algorithm/PearsonTest.java
@@ -11,6 +11,9 @@ public class PearsonTest {
 	@Test
 	public void testWriteCSV() throws Exception {
 		Pearson pearson = new Pearson();
+		
+		pearson.writeMatrixCSV();
+		
 		System.out.println("\nFinish the test: check if the pearson relationship "
 				+ "matrix has been writen in pearson_out_dir/pearson.csv");
 	}
